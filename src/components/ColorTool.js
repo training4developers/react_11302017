a978ee400717a12b0d7c8ee9491064efee678108
Eric W. Greene <eric@ericwgreene.com>
2017-11-30 14:03:52 -0500

    added first code demo

---
+++ src/components/ColorTool.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export class ColorTool extends React.Component {
+
+  render() {
+    // return React.createElement('h1', null, 'Color Tool!');
+    return (
+      <div>
+        <header>
+          <h1>Color Tool!!!</h1>
+        </header>
+      </div>
+    );
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { ColorTool } from './components/ColorTool';
+
+ReactDOM.render(
+  React.createElement(ColorTool),
+  document.querySelector('#root'),
+);
