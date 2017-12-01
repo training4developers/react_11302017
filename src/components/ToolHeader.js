077df2c35393976b193f0de44bb08f5e84cb7249
Eric W. Greene <eric@ericwgreene.com>
2017-12-01 17:46:59 -0500

    added code demos

--- src/components/ToolHeader.js
+++ src/components/ToolHeader.js
@@ -1,10 +1,22 @@
 import * as React from 'react';
 
-export class ToolHeader extends React.Component {
+export const ToolHeader = props =>
+  <header>
+    <h1>{props.headerText}</h1>
+  </header>;
 
-  render() {
-    return <header>
-      <h1>{this.props.headerText}</h1>
-    </header>;
-  }
-}
+// export const ToolHeader = props => {
+//   return <header>
+//     <h1>{props.headerText}</h1>
+//   </header>;
+// };
+
+
+// export class ToolHeader extends React.Component {
+  
+//     render() {
+//       return <header>
+//         <h1>{this.props.headerText}</h1>
+//       </header>;
+//     }
+//   }
