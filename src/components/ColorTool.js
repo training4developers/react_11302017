2aca7c2ae3bf94094f696d66acb8d1d7d57bb945
Eric W. Greene <eric@ericwgreene.com>
2017-11-30 19:42:37 -0500

    added code demos

--- src/components/ColorTool.js
+++ src/components/ColorTool.js
@@ -6,15 +6,29 @@ export class ColorTool extends React.Component {
     super(props);
 
     this.state = {
-      newColor: '',
+      colors: this.props.colors.concat(),
+      colorName: '',
+      colorHexCode: '',
     };
 
     this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   onChange(e) {
     this.setState({
-      [ e.target.name ]: e.target.value,
+      [ e.target.name ]: e.target.type === 'number'
+        ? Number(e.target.value)
+        : e.target.value
+    });
+  }
+
+  onClick() {
+    this.setState({
+      colors: this.state.colors.concat({
+        name: this.state.colorName,
+        hexCode: this.state.colorHexCode,
+      }),
     });
   }
 
@@ -26,14 +40,20 @@ export class ColorTool extends React.Component {
           <h1>{headerText.toUpperCase()}</h1>
         </header>
         <ul>
-          {this.props.colors.map(color =>  <li>{color}</li>)}
+          {this.state.colors.map(color => <li>{color.name}</li>)}
         </ul>
         <form>
           <div>
-            <label htmlFor="new-color-input">New Color</label>
-            <input type="text" id="new-color-input" name="newColor"
-              value={this.state.newColor} onChange={this.onChange} />
+            <label htmlFor="color-name-input">Color Name:</label>
+            <input type="text" id="color-name-input" name="colorName"
+              value={this.state.colorName} onChange={this.onChange} />
           </div>
+          <div>
+            <label htmlFor="color-hexcode-input">Color HexCode:</label>
+            <input type="text" id="color-hexcode-input" name="colorHexCode"
+              value={this.state.colorHexCode} onChange={this.onChange} />
+          </div>
+          <button type="button" onClick={this.onClick}>Add Color</button>
         </form>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import * as ReactDOM from 'react-dom';
 
 import { ColorTool } from './components/ColorTool';
 
-const colorList = [ 'red', 'blue', 'green', 'silver' ];
+const colorList = [
+  { name: 'red', hexCode: '#ff0000' },
+  { name: 'blue', hexCode: '#0000ff' },
+  { name: 'green', hexCode: '#00ff00' },
+];
 
 ReactDOM.render(
   <ColorTool colors={colorList} />,
